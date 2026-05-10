df3722f9d675d76b2bb420c1bd150449e603c1a5
Miruna Agopian <agopian.miruna@yahoo.com>
2026-05-10 21:14:54 +0200

    successfully tested all GET operations and displayed fetched data from all tables in the UI

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,18 +2,42 @@ import { Component, signal, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 //test, also inject is from the test!
 import { SurveyService } from './core/services/survey.service';
+import { QuestionService } from './core/services/question.service';
+import { OptionService } from './core/services/option.service';
+import { VoteService } from './core/services/vote.service';
+
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('PollApp');
-  //test
+  //TEST
   testSurveyVariable = inject(SurveyService);
-  ngOnInit(){
-      this.testSurveyVariable.getSingleSurvey("1");
+  testQuestionVariable = inject(QuestionService);
+  testOptionVariable = inject(OptionService);
+  testVoteVariable = inject(VoteService);
+  async ngOnInit() {
+    // 1. Load the test survey (optional)
+  await this.testSurveyVariable.getSingleSurvey('1');
+
+  // 2. Load the questions for survey 1
+  await this.testQuestionVariable.getQuestionsForSurvey('1');
+
+  // 3. Get the first question
+  const q = this.testQuestionVariable.questions()[0];
+  if (!q) return;
+
+  // 4. Load the options for that question
+  await this.testOptionVariable.getOptionsForQuestion(q.id.toString());
+
+  // 5. Load votes for first option
+  const secondOption = this.testOptionVariable.options()[1];
+  if (!secondOption) return;
+
+  await this.testVoteVariable.getVotesForOption(secondOption.id.toString());
   }
 }
