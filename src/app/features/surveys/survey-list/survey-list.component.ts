c4cb75f0c6f5093ed9bd27efd39bd13469f8e0f9
Miruna Agopian <agopian.miruna@yahoo.com>
2026-05-24 12:21:21 +0200

    finished functionalities on landing-page

--- src/app/features/surveys/survey-list/survey-list.component.ts
+++ src/app/features/surveys/survey-list/survey-list.component.ts
@@ -1,10 +1,12 @@
 import { Component, inject, Input } from '@angular/core';
 import { SurveyService } from '../../../core/services/survey.service';
 import { RouterLink } from '@angular/router';
+import { NgClass } from '@angular/common';
+import{ Survey } from '../../../core/interfaces/survey.interface'
 
 @Component({
   selector: 'app-survey-list',
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './survey-list.component.html',
   styleUrl: './survey-list.component.scss',
 })
@@ -14,54 +16,54 @@ export class SurveyList {
   @Input() filter: string | undefined;
   @Input() limit: number | undefined;
   @Input() sort: string | undefined;
+  @Input() secondaryStyle = false;
 
   ngOnInit() {
     this.surveyService.getAllSurveys();
   }
 
-  getfilteredSurveys() {
-    let list = [...this.surveyList()];
-    const now = new Date();
 
-    switch (this.filter) {
-      case 'ending-soon':
-        list = list.filter((s) => new Date(s.end_date) > now);
-        break;
+  private filterSurveys(list: Survey[]) {
+  const now = new Date();
 
-      case 'past':
-        list = list.filter((s) => new Date(s.end_date) < now);
-        break;
+  switch (this.filter) {
+    case 'ending-soon':
+      return list.filter((s) => new Date(s.end_date) > now);
 
-      default:
-        break;
-    }
+    case 'active':
+      return list.filter((s) => new Date(s.end_date) >= now);
 
-    if (this.sort === 'soonest-first') {
-      list = list.sort((a, b) => new Date(a.end_date).getTime() - new Date(b.end_date).getTime());
-    }
-    if (this.limit) {
-      list = list.slice(0, this.limit);
-    }
-    //filtering
-    switch (this.filter) {
-      case 'ending-soon':
-        list = list.filter((s) => new Date(s.end_date) > now);
-        break;
+    case 'past':
+      return list.filter((s) => new Date(s.end_date) < now);
 
-      case 'active':
-        list = list.filter((s) => new Date(s.end_date) >= now);
-        break;
-
-      case 'past':
-        list = list.filter((s) => new Date(s.end_date) < now);
-        break;
-
-      default:
-        break;
-    }
-
-    return list;
+    default:
+      return list;
   }
+}
+
+private sortSurveys(list: Survey[]) {
+  if (this.sort === 'soonest-first') {
+    return list.sort(
+      (a, b) =>
+        new Date(a.end_date).getTime() - new Date(b.end_date).getTime()
+    );
+  }
+  return list;
+}
+
+private limitSurveys(list: Survey[]) {
+  return this.limit ? list.slice(0, this.limit) : list;
+}
+
+getfilteredSurveys() {
+  let list = [...this.surveyList()];
+
+  list = this.filterSurveys(list);
+  list = this.sortSurveys(list);
+  list = this.limitSurveys(list);
+
+  return list;
+}
 
   calculateRemainingDays(serverDate: string) {
     const surveyDate = new Date(serverDate);
